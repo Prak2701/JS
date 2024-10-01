e3cae37413017c54469a77ed24ccac8d5ef7b597
Pratik Kulkarni <pratik.kulkarni@cirrius.com>
2024-10-01 18:06:03 +0530

    2 ways to add click event

--- JS_Practice/JS/ColorChanger.js
+++ JS_Practice/JS/ColorChanger.js
@@ -2,11 +2,39 @@ const button = document.querySelectorAll(".button")
 console.log(button);
 
 button.forEach(function(button) {
-    console.log(button);
+    button.addEventListener("click", function(e) {
+        const color = e.target.id;
+
+        switch(color){
+            case "grey": 
+            document.body.style.backgroundColor = color;
+            break;
+
+            case "green": 
+            document.body.style.backgroundColor = color;
+            break;  
+
+            case "blue":
+            document.body.style.backgroundColor = color;
+            break;  
+
+            case "red":
+            document.body.style.backgroundColor = color;  
+            break;
+
+            case "yellow":
+            document.body.style.backgroundColor = color;
+            break;
+        }
+    });
+});
+
+// button.forEach(function(button) {
+//     console.log(button);
     
-    button.addEventListener("click", function(e){
-        console.log(e.target.id);
+//     button.addEventListener("click", function(e){
+//         console.log(e.target.id);
     
-        document.body.style.backgroundColor = e.target.id;
-    })
-})
+//         document.body.style.backgroundColor = e.target.id;
+//     })
+// });
